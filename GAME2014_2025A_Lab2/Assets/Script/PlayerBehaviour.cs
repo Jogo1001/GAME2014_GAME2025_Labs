68a06156f79d991d9cfe51e77fdd6d452aa9fc75
Jogo1001 <101247726@georgebrown.ca>
2025-09-11 10:27:59 -0400

    boundaries script added

--- GAME2014_2025A_Lab2/Assets/Script/PlayerBehaviour.cs
+++ GAME2014_2025A_Lab2/Assets/Script/PlayerBehaviour.cs
@@ -10,7 +10,15 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     InputActionAsset _playerController;
 
-    public Vector2 Direction;   
+    public Vector2 Direction;
+
+
+
+    [SerializeField]
+    Boundary VerticalBoundary;
+
+    [SerializeField]
+    Boundary HorizontalBoundary;
 
     [SerializeField]
     public float speed;
@@ -25,8 +33,18 @@ public class PlayerBehaviour : MonoBehaviour
     {
         Direction = MoveInput.ReadValue<Vector2>();
         Debug.Log(Direction);
-        transform.position = new Vector3(transform.position.x + Direction.x * speed * Time.deltaTime,
-                                          transform.position.y + Direction.y * speed * Time.deltaTime
+        transform.position = new Vector3(transform.position.x + Direction.x * speed * Time.deltaTime
+                                         , transform.position.y + Direction.y * speed * Time.deltaTime
                                          , transform.position.z);
+
+
+        CheckBoundaries();
     }
+    public void CheckBoundaries()
+    {
+        transform.position = new Vector3 (Mathf.Clamp(transform.position.x, HorizontalBoundary.min, HorizontalBoundary.max),
+                                          Mathf.Clamp(transform.position.y, VerticalBoundary.min, VerticalBoundary.max),
+                                          transform.position.z);
+    }
+
 }
