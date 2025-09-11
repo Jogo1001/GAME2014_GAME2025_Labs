1bab83895ec31834515510703522a11ad7484370
Jogo1001 <101247726@georgebrown.ca>
2025-09-11 10:02:49 -0400

    Player Behavior With new inputsystem added

--- GAME2014_2025A_Lab2/Assets/Script/PlayerBehaviour.cs
+++ GAME2014_2025A_Lab2/Assets/Script/PlayerBehaviour.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
+    InputAction MoveInput;
+
+    [SerializeField]
+    InputActionAsset _playerController;
+
+    public Vector2 Direction;   
+
+    [SerializeField]
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MoveInput = _playerController.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Direction = MoveInput.ReadValue<Vector2>();
+        Debug.Log(Direction);
+        transform.position = new Vector3(transform.position.x + Direction.x * speed * Time.deltaTime,
+                                          transform.position.y + Direction.y * speed * Time.deltaTime
+                                         , transform.position.z);
     }
 }
