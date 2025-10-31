08f1a2f44246eaffdece296bc3070a5215edce91
Jogo1001 <101247726@georgebrown.ca>
2025-10-31 14:50:26 -0400

    player jumps and look to the movement direction

--- GAME2014_2025A_Lab4/Assets/Scripts/PlayerBehaviour.cs
+++ GAME2014_2025A_Lab4/Assets/Scripts/PlayerBehaviour.cs
@@ -13,21 +13,76 @@ public class PlayerBehaviour : MonoBehaviour
     float horizontalSpeed;
     [SerializeField]
     float maxHorizontalSpeed;
+
+    [SerializeField]
+    float jumpPower;
+
+    public bool isGrounded;
+
+    [SerializeField]
+    Transform groundPoint;
+    [SerializeField]
+    LayerMask groundLayerMask;
+
+    [SerializeField]
+    float groundCheckRadius;
     void Start()
     {
         moveInput = inputAsset.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundCheckRadius, groundLayerMask);
 
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        Move();
+        Jump();
+    }
+   
+    void Move()
+    {
         float xAxisValue = moveInput.ReadValue<Vector2>().x;
 
-        if(xAxisValue != 0)
+        if (xAxisValue != 0)
         {
             rb.AddForce(Vector2.right * xAxisValue * horizontalSpeed);
             rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxHorizontalSpeed, maxHorizontalSpeed);
+
+            CheckLookingDirection(xAxisValue);
         }
+
+
+   
+    }
+    void Jump()
+    {
+        float yAxisValue = moveInput.ReadValue<Vector2>().y;
+
+        if (isGrounded && yAxisValue > 0)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+    void CheckLookingDirection(float xValue)
+    {
+        if (xValue > 0)
+        {
+            transform.localScale = Vector3.one;
+
+        }
+        else if (xValue < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundPoint.position, groundCheckRadius);
     }
 }
